bbc205ace4dbd1e6fcaea5739debbd889484f9ca
Taras Golyk <tarasgolyk87@gmail.com>
2020-02-25 22:20:36 +0200

    Updates

---
+++ precache-manifest.f3e7223185429aa31ee8c2bdb6b8d17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185dfb5bace55aaeb41a16894c9bd78f",
+    "url": "/alco-quiz/index.html"
+  },
+  {
+    "revision": "f6db4e59553d6ad885a7",
+    "url": "/alco-quiz/static/css/main.cfb0e00c.chunk.css"
+  },
+  {
+    "revision": "720b92d946f48251d1dc",
+    "url": "/alco-quiz/static/js/2.65161ad0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/alco-quiz/static/js/2.65161ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6db4e59553d6ad885a7",
+    "url": "/alco-quiz/static/js/main.106ddd95.chunk.js"
+  },
+  {
+    "revision": "4c50a79e6c530a2bb4d6",
+    "url": "/alco-quiz/static/js/runtime-main.68ecfd87.js"
+  }
+]);
